11246608d750114871b602ad4887824206303652
yjhmelody <465402634@qq.com>
2018-08-03 21:08:14 +0800

    chap9:Applicative and Alternative

--- chap9-2.hs
+++ chap9-2.hs
@@ -140,8 +140,139 @@ class Functor f => Applicative f where
     pure :: a -> f a
     (<*>) :: f (a -> b) -> f a -> f a
 
+-- Applicative 类型类除了具有 Functor 的特性以外，能做的只是调用函子容器内的函数。因
+-- 此，常常把这个类型类称作可应用函子 Applicative Functor，意为可应用函数的函子，如
+-- Maybe 类型。
+
 instance Applicative Maybe where
     pure = Just
     Nothing <*> _ = Nothing
     (Just f) <*> arg = fmap f arg
 
+-- 为了更方便的表达这种计算，GHC 的库中定义了 liftA、liftA2、liftA3 函数，lift 意为
+-- 抬升，意思是将一个函数运算的参数分别放置于实现了 Applicative 类型类的容器中
+liftA :: Applicative f => (a -> b) -> f a -> f b
+liftA f a = pure f <*> a
+
+liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+liftA2 f a b = f <$> a <*> b
+
+(*>) :: f a -> f b -> f b
+u *> v = pure (const id) <*> u <*> v
+
+(<*) :: f a -> f b -> f a
+u <* v = pure const <*> u <*> v
+
+-- 在使用 *> 运算符时，先计算第一个参数并忽略其结果（这里注意忽略结果并不等于计算没
+-- 有意义），然后再计算第二个参数但将其返回为结果，<* 则刚好相反。用户也可以很容易地
+-- 使用 liftA2 函数来定义它们，然后应用 η 化简。
+(*>) = liftA2 (const id)
+(*<) = liftA2 const
+
+instance Applicative [] where
+    pure x = [x]
+    fs <*> xs = [f x | f <- fs, x <- xs]
+
+-- [(*),(+)] <*> [1,2] <*> [3,4]
+-- [3,4,6,8,4,5,5,6]
+
+
+-- 可应用函子 Applicative 是基于 Functor 的，也就是说它的下面有 3 个函数：
+-- 1 fmap0 :: a -> f a -- pure
+-- 2 fmap1 :: (a -> b) -> f a -> f b -- fmap
+-- 3 (<*>) :: f (a -> b) -> f a -> f b
+
+-- fmap0 与 fmap1 有一个序，很好理解，fmap0 的第 1 个参数是一个 0 元函数，而 fmap1 的
+-- 第 1 个参数是 1 元函数，然后我们这里定义一个 MultiFunctor 类型类去试着定义 fmap2、
+-- fmap3 等等：
+import Control.Applicative
+
+class MultiFunctor f where
+    fmap0 :: a -> f a -- pure
+    fmap1 :: (a -> b) -> f a -> f b -- fmap
+    fmap2 :: (a -> b -> c) -> f a -> f b -> f c
+    -- 有了前面的3个函数就可以定义任意多元的fmap函数了
+    fmap3 :: (a -> b -> c -> d) -> f a -> f b -> f c -> f d
+    fmap3 h x y z = fmap2 ($) (fmap2 h x y) z
+    fmap4 :: (a -> b -> c -> d -> e) -> f a -> f b -> f c -> f d -> f e
+    fmap4 h w x y z = fmap2 ($) (fmap3 h w x y) z
+    fmap5 :: (a -> b -> c -> d -> e -> g) -> f a -> f b -> f c -> f d -> f e -> f g
+    fmap5 h v w x y z = fmap2 ($) (fmap4 h v w x y) z
+
+-- 后面的 fmap3、fmap4、fmap5 以及更多元的映射函数 fmap 都可以用 fmap2 与少 1 元的 
+-- fmap 函数来定义，即 fmapn f a1 a2 · · · an = fmap2 ($) (fmap(n−1) f a1 a2 · · · an−1) an。
+
+-- 我们就可以看到这里定义的多函子类型类 MultiFunctor 本质上就是可应用函子
+-- Applicative 类型类，它的本质就是可以把任意多元的函数应用到任意多个放在 f 容器里
+-- 的参数上，把一般的函数应用提升到了某种参数化的类型 f 上，比如 Maybe、列表等等。
+-- Control.Applicative 模块中定义的 liftA、liftA2、liftA3 对应的就是第 1 个参数为 1 元、
+-- 2 元和 3 元函数的 fmap，lift 就是提升的意思。
+
+(<<*>>) :: f1 (f2 (a -> b)) -> f1 (f2 a) -> f1 (f2 b)
+(<<*>>) = liftA2 (<*>)
+-- > Just [(+1), (+6)] <<*>> Just [1, 2]
+-- Just [2, 3, 7, 8]
+
+
+-- 选择可应⽤函⼦ Alternative
+-- Alternative 意为可二选一的、后备的。这个类型类依赖于 Applicative
+-- 类型类，并且在 Control.Applicative 中是这样定义的
+
+infixl 3 <|>
+
+class Applicative f => Alternative f where
+    empty :: f a
+    (<|>) :: f a -> f a -> f a
+
+-- 这个类型类中定义的运算符 <|> 意为其他的选择。比如，当给定多个值的时候，需要选择合
+-- 理的，如下：
+-- > Nothing <|> Nothing <|> Just 1 <|> Just 2
+-- Just 1
+
+instance Alternative Maybe where
+    empty = Nothing
+    Nothing <|> p = p
+    Just x <|> _ = Just x
+
+-- 通过上面的函数计算与 <|> 的定义，应该可以看出，empty 定义的为 <|> 运算符的单位
+-- 元，正如 <*> 运算符的单位元为 pure id 一样。
+
+-- 列表类型实现 Alternative 类型类实例时，<|> 运算符应该与 ++ 运算符是等价的。
+instance Alternative [] where
+    empty = []
+    (<|>) = (++)
+    some :: f a -> f [a]
+    some v = some_v
+        where
+            many_v = some_v <|> pure []
+            some_v = (:) <$> v <*> many_v
+
+    many :: f a -> f [a]
+    many v = many_v
+        where
+            many_v = some_v <|> pure []
+            some_v = (:) <$> v <*> many_v
+        
+    optional :: Alternative f => f a -> f (Maybe a)
+    optional v = Just <$> v <|> pure Nothing
+
+-- Alternative 类型类中还定义好了另外两个函数 some 与 many 定义，使用了 Applicative
+-- 的 <*> 与 Alternative 的 <|> 运算符
+
+-- some 函数先检察 v 值是否为 empty
+-- 单位元，如果为单位元，那么返回这个单位元，否则返回 v 内的值多次，即尝试返回 v 内的
+-- 值 1 次或者更多次。而 many 函数先判定 v 值是否为 empty 单位元，如果为单位元，那么返
+-- 回 []，意为返回 v 内的值 0 次或者更多次。而定义中的本地函数 many_v 与 some_v 为互调
+-- 递归定义，若 some_v 不能返回 1 次以上，则返回 []，这正是 many 的意思。同理，将 v 内
+-- 的值返回一次，然后再返回多次也正是 some_v 的意思。
+
+-- 如果在 GHCi 中运行 some (Just 5)，那么函数的运行不会停止。因为 5 为一
+-- 个纯数值，所以它的计算在 Haskell 中永远不会失败，其结果可以理解为 Just [5,5,5,5..]。
+-- many (Just 5) 的结果就与 some (Just 5) 相同了，它从 Just 5 中一直取 5 而不会停止。
+
+-- 库中还定义了 optional 函数，即返回 v 中的值 0 次或者 1 次，如果没有值，则会返回f Nothing。
+-- > optional [1,2,3]
+-- [Just 1,Just 2,Just 3,Nothing]
+
+
+-- 上面的函子、可应用函子、可选择应用函子 3 个类型类是来自于抽象代数的
